4fa6b7598bc6414e9234f6f3c8270768eeb7ba4d
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2022-01-06 01:53:59 +0100

    Update HeaderBarBrand.js

--- react-app/src/components/HeaderBarBrand.js
+++ react-app/src/components/HeaderBarBrand.js
@@ -12,9 +12,9 @@ const HeaderBarBrand = () => (
       <i className="fab js-logo fa-react fa-2x" aria-hidden="true" />
     </a>
     <NavLink to="/" className="navbar-item nav-home">
-      <span className="brand-first">MY</span>
-      <span className="brand-second">SHOPPING</span>
-      <span className="brand-third">LIST</span>
+      <span className="brand-first">KJELL</span>
+      <span className="brand-second">MAEKELBERG</span>
+      <span className="brand-third">SHOPPING LIST</span>
     </NavLink>
   </div>
 );
